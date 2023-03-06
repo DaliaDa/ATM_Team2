59907367e4bd7d075dbe05596bdb57a91eaf20ec
DaliaDa <dalia.dauciuniene@gmail.com>
2023-03-06 23:13:00 +0200

    commiting

--- Dalia.py
+++ Dalia.py
@@ -1,9 +1,1 @@
-Withdraw = input("Enter the amount: ")
-print (Withdraw, "$ Have been in your account")
-
-Exit = input("Would you like to continuous? Y/N: ")
-if Exit == "N":
-    print("Thank you")
-break
-else:
-continue
+print("omg")
