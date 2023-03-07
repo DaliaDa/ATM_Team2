1970cc56ad3826bf35b811651348af6347bf57a0
evijastasko <evija.stasko@gmail.com>
2023-03-07 14:26:14 +0200

    deposit money / changes

--- Deposit.py
+++ Deposit.py
@@ -2,7 +2,7 @@ import Operations as ops
 
 operation = int(input("Enter 1 to deposit\n"))
 if operation ==1: #what will be the operation #?
-    amount_deposit = input("Please insert banknotes in the ATM (Eur 5, Eur 10, Eur 20, Eur 50, Eur 100 bills are accepted) and Enter the amount of deposit:\n ")
+    amount_deposit = input("Please insert banknotes in the ATM (Eur 5, Eur 10, Eur 20, Eur 50, Eur 100 bills are accepted) and Enter the amount of deposit:\n")
     deposit = 0
     try:
         deposit = int(amount_deposit)
