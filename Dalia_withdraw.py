8aaaf1b9ecf69d15f971f667a19ce23088373735
DaliaDa <dalia.dauciuniene@gmail.com>
2023-03-10 22:31:07 +0200

    Withdraw

--- Dalia_withdraw.py
+++ Dalia_withdraw.py
@@ -2,7 +2,9 @@ import Operations as ops
 def Withdraw_function():
    Option = input("If you want to WITHDRAW money, press w: ")
    if Option == "w":
+      print("Your balance is", ops.get_balance(), "Eur")
       Withdraw = input("Enter the amount you would like to withdraw: ")
+
       try:
          if int(ops.get_balance()) < int(Withdraw):
             print("Error1")
@@ -10,14 +12,14 @@ def Withdraw_function():
       except:
          print("Not enough money")
          quit()
+
       print(Withdraw, "Eur have been withdrawn from your account")
-   Exit = input("Would you like to exit? y/n: ")
+      print("Your new balance is", ops.get_balance() - int(Withdraw), "Eur")
+   Exit = input("Would you like to EXIT? y/n: ")
    if Exit == "y":
       print("Thank you for Using ATM. Please take your card.")
    else:
       print("Choose another operation")
-
-
 
 
 Withdraw_function()
